5f892882768ad0ba3fa43bdb2634cfe41a1e2145
H9ko <grmh9ko@gmail.com>
2021-10-13 16:59:34 +0300

    small_fix

--- components/pages/game/components/GamePage.tsx
+++ components/pages/game/components/GamePage.tsx
@@ -10,6 +10,10 @@ import { RESULT } from 'config/routes';
 
 import IQuestion from 'interfaces/questionType';
 
+import ErrorMessage from 'components/shared/atoms/ErrorMessage';
+import ErrorDecorator from 'decorators/ErrorDecorator';
+import { useGameQuestions } from 'lib/apollo/hooks/state/game';
+import { LIMIT_QUESTIONS } from './constants';
 import GameStep from './GameStep';
 
 const getCurrentQuestion = (questions: IQuestion[], index: number) => {
@@ -23,25 +27,26 @@ const getCurrentQuestion = (questions: IQuestion[], index: number) => {
   return { ...question, options };
 };
 
-const GamePage = ({ questions }) => {
+const GamePage = () => {
+  const { questions, loading, error } = useGameQuestions({ limitQuestions: LIMIT_QUESTIONS });
   const [answers, setAnswers] = useState<IQuestion[]>([]);
   const [step, setStep] = useState(0);
   const router = useRouter();
-
   const endGame = () => {
     console.log('Место для запуска мутации с записью ответов ---  ', answers);
     router.push(RESULT);
   };
-
-  if (questions && step >= questions.length) {
+  if (questions.length > 0 && step >= questions.length) {
     endGame();
   }
-
   const addAnswer = (question: IQuestion) => (answer: string) => {
     setAnswers((prevAnswers) => [...prevAnswers, { ...question, answer }]);
     setStep(step + 1);
   };
   const currentQuestion = getCurrentQuestion(questions, step);
+
+  if (loading) return <h3>Loading...</h3>;
+  if (error) return <ErrorMessage>{new ErrorDecorator(error).getMessages()}</ErrorMessage>;
   return currentQuestion ? (
     <GameStep question={currentQuestion} endGame={endGame} addAnswer={addAnswer(currentQuestion)} />
   ) : (
