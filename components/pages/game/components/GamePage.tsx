26533ab321017d3d559fbd5e724fdf0dbbee2469
H9ko <grmh9ko@gmail.com>
2021-10-13 22:15:34 +0300

    fix loading, add type for GamePage

--- components/pages/game/components/GamePage.tsx
+++ components/pages/game/components/GamePage.tsx
@@ -9,11 +9,6 @@ import WithAuthSecurity from 'lib/auth/withAuthSecurity';
 import { RESULT } from 'config/routes';
 
 import IQuestion from 'interfaces/questionType';
-
-import ErrorMessage from 'components/shared/atoms/ErrorMessage';
-import ErrorDecorator from 'decorators/ErrorDecorator';
-import { useGameQuestions } from 'lib/apollo/hooks/state/game';
-import { LIMIT_QUESTIONS } from './constants';
 import GameStep from './GameStep';
 
 const getCurrentQuestion = (questions: IQuestion[], index: number) => {
@@ -27,8 +22,7 @@ const getCurrentQuestion = (questions: IQuestion[], index: number) => {
   return { ...question, options };
 };
 
-const GamePage = () => {
-  const { questions, loading, error } = useGameQuestions({ limitQuestions: LIMIT_QUESTIONS });
+const GamePage = ({ questions }: { questions: IQuestion[] }) => {
   const [answers, setAnswers] = useState<IQuestion[]>([]);
   const [step, setStep] = useState(0);
   const router = useRouter();
@@ -45,8 +39,6 @@ const GamePage = () => {
   };
   const currentQuestion = getCurrentQuestion(questions, step);
 
-  if (loading) return <h3>Loading...</h3>;
-  if (error) return <ErrorMessage>{new ErrorDecorator(error).getMessages()}</ErrorMessage>;
   return currentQuestion ? (
     <GameStep question={currentQuestion} endGame={endGame} addAnswer={addAnswer(currentQuestion)} />
   ) : (
