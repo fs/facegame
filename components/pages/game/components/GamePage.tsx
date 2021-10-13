becd003d390358a623af9d9edf6068d5b4e14c5d
nadezhdakharchuk <nadezhda.kharchuk@flatstack.com>
2021-10-13 15:30:31 +0300

    fix ssr double requests

--- components/pages/game/components/GamePage.tsx
+++ components/pages/game/components/GamePage.tsx
@@ -1,17 +1,16 @@
 import React, { useState } from 'react';
+import { useRouter } from 'next/router';
+import shuffle from 'lodash/shuffle';
 
 import WithAuth from 'lib/auth/withAuth';
 import { withApolloClient } from 'lib/withApolloClient';
 import WithAuthSecurity from 'lib/auth/withAuthSecurity';
 
-import shuffle from 'lodash/shuffle';
 import { RESULT } from 'config/routes';
 
-import { useRouter } from 'next/router';
-import { useGameQuestions } from 'lib/apollo/hooks/state/game';
 import IQuestion from 'interfaces/questionType';
+
 import GameStep from './GameStep';
-import { LIMIT_QUESTIONS } from './constants';
 
 const getCurrentQuestion = (questions: IQuestion[], index: number) => {
   const question = questions[index];
@@ -24,18 +23,20 @@ const getCurrentQuestion = (questions: IQuestion[], index: number) => {
   return { ...question, options };
 };
 
-const GamePage = () => {
-  const { questions } = useGameQuestions({ limitQuestions: LIMIT_QUESTIONS });
+const GamePage = ({ questions }) => {
   const [answers, setAnswers] = useState<IQuestion[]>([]);
   const [step, setStep] = useState(0);
   const router = useRouter();
+
   const endGame = () => {
     console.log('Место для запуска мутации с записью ответов ---  ', answers);
     router.push(RESULT);
   };
+
   if (questions && step >= questions.length) {
     endGame();
   }
+
   const addAnswer = (question: IQuestion) => (answer: string) => {
     setAnswers((prevAnswers) => [...prevAnswers, { ...question, answer }]);
     setStep(step + 1);
