caff28297af226da10a2a1a120dca8deab75ecce
Vladimir Afanasiev <57991929+VladimirAfanasievFS@users.noreply.github.com>
2021-10-18 10:10:35 +0300

    Add tests use game process (#14)
    * add local state, change header
    
    * add createResult mutation
    
    * fix HoverIcon
    
    * add tests useGameProcess
    
    Co-authored-by: H9ko <grmh9ko@gmail.com>

--- interfaces/questionType.ts
+++ interfaces/questionType.ts
@@ -1,11 +1,8 @@
-import { Colors } from 'public/styles/theme';
-
 export default interface IQuestion {
   id: number;
   name: string;
   fullName: string;
   wrongAnswers: string[];
-  answerStatusColor: Colors;
   options: string[];
   answer?: string;
 }
