65b9c3598c41c9e4703bbbe6b5ba53511e67f2e3
nadezhdakharchuk <nadezhda.kharchuk@flatstack.com>
2021-11-15 09:38:51 +0300

    fix

--- config/routes.ts
+++ config/routes.ts
@@ -2,7 +2,3 @@ export const HOME = '/';
 export const LOGIN = '/login';
 export const GAME = '/game';
 export const RESULT = '/result';
-export const PROFILE = '/profile';
-export const FORM_EXAMPLES = '/form-examples';
-export const ACTIVITY = '/activity';
-export const STATIC_PAGE = '/static-page';
