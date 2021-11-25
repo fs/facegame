5fe834d499a81264b741472ae62e02c7f8c3dbde
Olga Urentseva <olga.urentseva@flatstack.dev>
2021-11-25 13:27:34 +0100

    replace timer from GameStep to Game

--- components/pages/game/components/Game.tsx
+++ components/pages/game/components/Game.tsx
@@ -1,13 +1,37 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
+import { RESULT } from 'config/routes';
 import DefaultTemplate from 'components/shared/templates/DefaultTemplate';
+import TimerBar from 'components/shared/atoms/TimerBar';
 import useSendAnswerAndGetNextQuestion from 'lib/apollo/hooks/actions/sendAnswerAndGetNextQuestion';
+import { useRouter } from 'next/router';
 import GameStep from './GameStep';
 import HeaderChildren from './HeaderChildren';
+import { TitleDescription } from './styled';
 
 interface Question {
   answerOptions: string[];
   avatarUrl: string;
+}
+
+const FULL_BAR = 100;
+const FULL_TIME = 45;
+
+function useTimer(limit: number, cb = console.log): number {
+  const [time, setTime] = useState(limit);
+  const onceCallRef = useRef(false);
+  if (time <= 0 && onceCallRef.current === false) {
+    cb();
+    onceCallRef.current = true;
+  }
+  useEffect(() => {
+    function tick() {
+      setTime((prevTime) => (prevTime === 0 ? 0 : prevTime - 1));
+    }
+    const id = setInterval(tick, 1000);
+    return () => clearInterval(id);
+  }, [time]);
+  return time;
 }
 
 const Game = ({ initialQuestion, gameId }: { initialQuestion: Question; gameId: string }) => {
@@ -15,6 +39,7 @@ const Game = ({ initialQuestion, gameId }: { initialQuestion: Question; gameId: 
   const [currentAnswer, setCurrentAnswer] = useState<string | undefined>(undefined);
 
   const [sendAnswerAndGetNextQuestion, sendAnswerAndGetNextQuestionState] = useSendAnswerAndGetNextQuestion();
+  const router = useRouter();
 
   const isCurrentAnswerCorrect =
     !sendAnswerAndGetNextQuestionState.loading &&
@@ -58,15 +83,25 @@ const Game = ({ initialQuestion, gameId }: { initialQuestion: Question; gameId: 
     setCurrentAnswer(value);
   };
 
+  const endGame = () => {
+    router.push(RESULT);
+  };
+  const currentSecond = useTimer(FULL_TIME, endGame);
+
+  const barWidth = (FULL_BAR * currentSecond) / FULL_TIME;
+
   return (
     <DefaultTemplate
       title="What is the name of that superhero?"
       headerChildren={
         <HeaderChildren
+          endGame={endGame}
           correctAnswersCount={sendAnswerAndGetNextQuestionState.data?.sendAnswerAndGetNextQuestion.correctAnswersCount}
         />
       }
     >
+      <TitleDescription>What is the name of that superhero?</TitleDescription>
+      <TimerBar time={currentSecond} width={barWidth} />
       <GameStep
         question={currentQuestion}
         answer={answer}
--- components/pages/game/components/GameStep.tsx
+++ components/pages/game/components/GameStep.tsx
@@ -1,14 +1,9 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React from 'react';
 import Question from 'domain/Question';
 import { component as CorrectIcon } from 'public/images/icons/correct.svg';
 import { component as IncorrectIcon } from 'public/images/icons/incorrect.svg';
 
-import TimerBar from 'components/shared/atoms/TimerBar';
-
-import { PageContent, Content, PreviewImg, ButtonForAnswer, ButtonForQuestion, TitleDescription } from './styled';
-
-const FULL_BAR = 100;
-const FULL_TIME = 45;
+import { PageContent, Content, PreviewImg, ButtonForAnswer, ButtonForQuestion } from './styled';
 
 type Step = {
   question: Question;
@@ -17,35 +12,11 @@ type Step = {
   currentAnswer: string | undefined;
 };
 
-function useTimer(limit: number, cb = console.log): number {
-  const [time, setTime] = useState(limit);
-  const onceCallRef = useRef(false);
-  if (time <= 0 && onceCallRef.current === false) {
-    cb();
-    onceCallRef.current = true;
-  }
-  useEffect(() => {
-    function tick() {
-      setTime((prevTime) => (prevTime === 0 ? 0 : prevTime - 1));
-    }
-    const id = setInterval(tick, 1000);
-    return () => clearInterval(id);
-  }, [time]);
-  return time;
-}
-
 const GameStep = ({ question, answer, isCurrentAnswerCorrect, currentAnswer }: Step) => {
   const isShowResultAnswer = Boolean(currentAnswer);
 
-  const endGame = () => {};
-  const currentSecond = useTimer(FULL_TIME, endGame);
-
-  const barWidth = (FULL_BAR * currentSecond) / FULL_TIME;
-
   return (
     <PageContent data-testid="page-content">
-      <TitleDescription>What is the name of that superhero?</TitleDescription>
-      <TimerBar time={currentSecond} width={barWidth} />
       <PreviewImg key={question.avatarUrl} src={question.avatarUrl} />
       <Content>
         {question.answerOptions.map((option) =>
